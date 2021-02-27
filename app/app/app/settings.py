28532220bbbded32d9cf432f5bfd1209b54e86b5
joeDiHare <stefano286@gmail.com>
2021-02-27 14:09:47 -0800

    runtime

--- app/app/app/settings.py
+++ app/app/app/settings.py
@@ -97,6 +97,8 @@ DATABASES = {
         'PORT': '',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
