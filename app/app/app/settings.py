87caafb7e76f501a9204e0e6beacbe95241598e4
joeDiHare <stefano286@gmail.com>
2021-02-27 14:25:25 -0800

    att2

--- app/app/app/settings.py
+++ app/app/app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'zq5b!jy!vx@xd-pm+q&h6^v)%(7394*fi6y^=3w#j331gyh%pp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'plpayerevals.herokuapp.com', '.herokuapp.com']
 
 
 # Application definition
@@ -97,8 +97,8 @@ DATABASES = {
         'PORT': '',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -134,8 +134,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+PROJECT_ROOT =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL   = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 #  To serve files directly from their original locations (usually in STATICFILES_DIRS
 #  or app static subdirectories) without needing to be collected into STATIC_ROOT
